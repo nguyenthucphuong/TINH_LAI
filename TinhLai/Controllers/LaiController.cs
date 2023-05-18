463fdc16b995a418d536e6d37f49c02865b76ce7
nguyenthucphuong <thuphucthanh@gmail.com>
2023-05-18 16:42:53 +0700

    Add project files.

---
+++ TinhLai/Controllers/LaiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinhLai.Controllers
+{
+    public class LaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Index(string sotien, double laisuat, uint kyhan)
+        {
+            string sotien_string = sotien.Replace(",", ""); // loại bỏ ký tự phân cách
+            int sotien_number = int.Parse(sotien_string);
+            ViewData["sotien"] = sotien;
+            ViewData["laisuat"] = laisuat;
+            ViewData["kyhan"] = kyhan;
+            double tienlai = sotien_number * kyhan * laisuat/1200;
+            double tongtien = sotien_number + tienlai;
+            ViewData["tienLai"] = tienlai.ToString(tienlai % 1 == 0 ? "N0" : "N2");
+            ViewData["tongTien"] = tongtien.ToString(tongtien % 1 == 0 ? "N0" : "N2");
+            return View();
+        }
+    }
+}
---
+++ TinhLai/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    const inputFields = $('input[type="number"], input[type="text"]');
+    inputFields.on('input', function () {
+        let number = parseInt($(this).val().replace(/,/g, '').replace(/[^0-9]/g, ''));
+        if (isNaN(number)) {
+            number = 0;
+        }
+        $(this).val(number.toLocaleString('en-US'));
+    });
+
+    $('#ketqua').on('click', function () {
+        $('form')[0].submit();
+    });
+});
